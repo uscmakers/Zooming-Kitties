e8f0060d042b350557fc1d35662f06aedb99afa5
Irith Katiyar <imkatiya@usc.edu>
2023-03-29 19:20:00 -0700

    Update motor_control_basic.py

--- motor_control_basic.py
+++ motor_control_basic.py
@@ -55,7 +55,7 @@ def face_detect():
 
     window_title = "Face Detect"
     face_cascade = cv2.CascadeClassifier(
-        "fist.xml"
+        "/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"
     )
     video_capture = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
     if video_capture.isOpened():
