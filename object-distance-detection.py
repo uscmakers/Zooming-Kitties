30663cbe0f4425931fa81f1f8f927ce6e4ef6afb
Irith Katiyar <imkatiya@usc.edu>
2023-04-18 21:01:50 -0700

    Updated import from repo

--- object-distance-detection.py
+++ object-distance-detection.py
@@ -8,7 +8,7 @@ import imutils
 from imutils import paths
 import numpy as np
 import cv2
-import apriltag
+import apriltag.python.apriltag.py as apriltag
 import copy
 
 ### CONSTANTS ###
@@ -18,16 +18,6 @@ connection_string = '/dev/ttyACM0'
 KNOWN_WIDTH = 11
 KNOWN_HEIGHT = 8.5
 FOCAL_LENGTH = 1 # TODO: calibrate
-
-at_detector = apriltag.Detector( 
-	families="tag36h11",
-	nthreads=1,
-	quad_decimate=1.0,
-	quad_sigma=0.0,
-	refine_edges=1,
-	decode_sharpening=0.25,
-	debug=0
-) # April Tag detector
 
 def gstreamer_pipeline(
     capture_width=1920,
@@ -64,38 +54,16 @@ def distance_to_camera(W, F, P):
 
 # TODO: Make angle helper function
 
-# COPIED FROM [https://github.com/Kazuhito00/AprilTag-Detection-Python-Sample/blob/main/sample.py]
-def draw_tags(image, tags):
-    for tag in tags:
-        center = tag.center
-        corners = tag.corners
-
-        center = (int(center[0]), int(center[1]))
-        corner_01 = (int(corners[0][0]), int(corners[0][1]))
-        corner_02 = (int(corners[1][0]), int(corners[1][1]))
-        corner_03 = (int(corners[2][0]), int(corners[2][1]))
-        corner_04 = (int(corners[3][0]), int(corners[3][1]))
-
-        cv2.circle(image, (center[0], center[1]), 5, (0, 0, 255), 2)
-
-        cv2.line(image, (corner_01[0], corner_01[1]),
-                (corner_02[0], corner_02[1]), (255, 0, 0), 2)
-        cv2.line(image, (corner_02[0], corner_02[1]),
-                (corner_03[0], corner_03[1]), (255, 0, 0), 2)
-        cv2.line(image, (corner_03[0], corner_03[1]),
-                (corner_04[0], corner_04[1]), (0, 255, 0), 2)
-        cv2.line(image, (corner_04[0], corner_04[1]),
-                (corner_01[0], corner_01[1]), (0, 255, 0), 2)
-    
-    return image
-
 ### MAIN ROUTINE ###
 
 def main():
+
 	vehicle = connect(connection_string, wait_ready=True, baud=115200, timeout=60)
 	print("Successfully connected to vehicle at " + connection_string + "!")
 	vehicle.armed = True
 	time.sleep(1)
+
+	at_detector = apriltag.Detector()
 
 	window_title = "Object Distance Detection"
 	video_capture = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
@@ -112,7 +80,6 @@ def main():
        			# Get list of april tags
 				tags = at_detector.detect(image)
        			# Draw tags
-				dbg_image = draw_tags(dbg_image, tags)
 				if(len(tags) > 0):
 					print("detected")
 				else:
