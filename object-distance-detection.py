9beb2c5561fbb71909804ced2c505603055cb8d0
Irith Katiyar <imkatiya@usc.edu>
2023-04-25 16:44:14 -0700

    Changed

--- object-distance-detection.py
+++ object-distance-detection.py
@@ -13,8 +13,8 @@ import apriltag.python.apriltag as apriltag
 
 ### CONFIGURATION ###
 
-connect_to_vehicle = True # enables/disables code dependent on Pixhawk connection (set to True for integrated testing)
-show_window = False # enables/disables camera feed window appearing on monitor (set to False for integrated testing)
+connect_to_vehicle = False # enables/disables code dependent on Pixhawk connection (set to True for integrated testing)
+show_window = True # enables/disables camera feed window appearing on monitor (set to False for integrated testing)
 
 ### CONSTANTS ###
 
